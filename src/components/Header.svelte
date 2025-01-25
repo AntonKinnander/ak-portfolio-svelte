b5d2ef0e3df2c506487950b911ee24bb3b9e4ef1
AntonKinnander <a.kinnander@icloud.com>
2025-01-25 19:14:54 +0100

    eee

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -11,6 +11,8 @@
   import { scrollToSection } from "./utils/scroll-utils.js";
   gsap.registerPlugin(ScrollTrigger);
 
+  import AkLogo from "./svg/ak-logo.svelte";
+
   let headerHeight = 0;
   onMount(() => {
     let scrollTween;
@@ -23,7 +25,10 @@
 
     function goToSection(index) {
       scrollTween = gsap.to(window, {
-        scrollTo: { y: index * (window.innerHeight - headerHeight), autoKill: false },
+        scrollTo: {
+          y: index * (window.innerHeight - headerHeight),
+          autoKill: false,
+        },
         duration: 1,
         onComplete: () => (scrollTween = null),
         overwrite: true,
@@ -40,31 +45,51 @@
 </script>
 
 <header
-  id="header" class={"sticky h-7 z-[10] top-0 duration-200 px-1 py-0.5 flex items-center justify-start gap-6 backdrop-blur-lg border-b border-solid border-[var(--activeFG)]"}
+  id="header"
+  class={"sticky h-7 z-[10] top-0 duration-200 px-1 py-0.5 flex items-center justify-start gap-6 backdrop-blur-lg border-b border-solid border-[var(--activeFG)]"}
 >
- <a href="" on:click={(event) => { event.preventDefault(); scrollToSection('home'); }}  ><h4 class="font-medium flex gap-3 items-center hover:text-[var(--activeBG)]">
-    <!-- Current page title instead of name, add dropdown menu -->
-    <img src="./assets/ak-logo.svg" class="h-5" alt="logo" /> Anton Kinnander 
-  </h4>
-</a> 
+  <a
+    href=""
+    on:click={(event) => {
+      event.preventDefault();
+      scrollToSection("home");
+    }}
+    ><h4
+      class="font-medium flex gap-3 items-center hover:text-[var(--activeBG)]"
+    >
+      <!-- Current page title instead of name, add dropdown menu -->
+      <!-- <img src="./assets/ak-logo.svg" class="h-5" alt="logo" />  -->
+      <div class="h-5"><AkLogo class="h-5" /></div>
+      Anton Kinnander
+    </h4>
+  </a>
   <div
     class="flex flex-initial flex-row items-center gap-4 justify-start hidden md:block"
   >
-
-
     {#each tabs as tab}
-    {#if tab.type !== 3}
-    {#if tab.section}
-    <a href="{tab.link}" class="hover:text-[var(--activeBG)] mr-4" on:click={(event) => { event.preventDefault(); scrollToSection(tab.link); }}> {tab.name}</a>
-  
-    {:else}
-    <a class="hover:text-[var(--activeBG)] mr-4" href={tab.link} target="_blank">
-      {tab.name}
-    </a>
-    {/if}
-    {/if}
-  {/each}
-
+      {#if tab.type !== 3}
+        {#if tab.section}
+          <a
+            href={tab.link}
+            class="hover:text-[var(--activeBG)] mr-4"
+            on:click={(event) => {
+              event.preventDefault();
+              scrollToSection(tab.link);
+            }}
+          >
+            {tab.name}</a
+          >
+        {:else}
+          <a
+            class="hover:text-[var(--activeBG)] mr-4"
+            href={tab.link}
+            target="_blank"
+          >
+            {tab.name}
+          </a>
+        {/if}
+      {/if}
+    {/each}
   </div>
   <!-- Spacing -->
   <p class="ml-auto px-1"></p>
